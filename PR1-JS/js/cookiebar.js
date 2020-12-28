c29823cae1e076b7ee96ed571d6a6751a9d3148b
malinstarvall <malinstarvall@users.noreply.github.com>
2020-12-28 13:35:45 +0100

    cookie

--- PR1-JS/js/cookiebar.js
+++ PR1-JS/js/cookiebar.js
@@ -1,2 +1,16 @@
 // COOKIEBANNER
-   
+   function showToggle() {
+
+	// gets the tag that has the ID
+	var divEle = document.getElementById("hide");
+
+	// check it is display none
+	if (divEle.style.display === "none") {
+		// if it is, make it display block
+		divEle.style.display = "block";
+	} else {
+		// if no make it display none
+		divEle.style.display = "none";
+	}
+
+}
